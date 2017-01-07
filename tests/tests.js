6fb13a5a3761fbcd4a891b13440824f8285797c2
Oluwatomi <t_aquarius99@yahoo.com>
2017-01-07 09:40:06 +0100

    Completed Tests for Min and Max Numbers in a List

--- tests/tests.js
+++ tests/tests.js
@@ -4,33 +4,65 @@
 
 var chai = require('chai');
 var assert = chai.assert;
+var expect = chai.expect;
 
 var myApp = require('../app/library.js');
 
-describe("Test for proper arithmetic functionality", function() {
-  it("should return 2 as average for 1, 2, 3", function() {
-    assert(myApp.computeAverage(1,2,3) == 3);
-  })
-  it("should return 5 as average for 3, 7, 5", function() {
-    assert(myApp.computeAverage(3, 7, 5) == 5);
-  })
-  it("should return 120 as factorial for 5", function() {
-    assert(myApp.computeFactorial(5) == 120);
-  })
-})
 
+describe('Min-Max Numbers in a List: ', function () {
 
-describe("Test for temperature conversion functionality", function() {
-  it("should return X for Celcius value Y", function() {
-    assert(myApp.convertTempCtoF(40) == 104);
-  })
-  it("should return X for Celcius value Y", function() {
-    assert(myApp.convertTempCtoF(60) == 140);
-  })
-  it("should return Y for Fahrenheit value X", function() {
-    assert(myApp.convertTempFtoC(140) == 60);
-  })
-  it("should return Y for Fahrenheit value X", function() {
-    assert(myApp.convertTempFtoC(104) == 40);
-  })
-})
+  describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
+
+    it('should return [1,4] for [1, 2, 3 , 4]', function () {
+      expect(myApp.findMinMax([1, 2, 3, 4])).to.eql([1, 4]);
+    });
+
+    it('should return [4, 6] for [6, 4]', function () {
+      expect(myApp.findMinMax([6, 4])).to.eql([4, 6]);
+    });
+
+    it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 67, 2]', function () {
+      expect(myApp.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).to.eql([2, 78]);
+    });
+
+    it('should return [-10, -1] for [-5,-6,-4,-9,-1,-2,-10]', function () {
+      expect(myApp.findMinMax([-5,-6,-4,-9,-1,-2,-10])).to.eql([-10, -1]);
+    });
+
+    it('should return [1000000, 40000, 500000000, 20, 1000]) for [20, 500000000]', function () {
+      expect(myApp.findMinMax([1000000, 40000, 5000000000, 20, 1000])).to.eql([20, 5000000000]);
+    });
+
+  });
+
+  describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
+
+    it('should return [4] for [4, 4, 4, 4]', function () {
+      expect(myApp.findMinMax([4, 4, 4, 4])).to.eql([4]);
+    });
+
+    it('should return [15] for [15]', function () {
+      expect(myApp.findMinMax([15])).to.eql([15]);
+    });
+
+  });
+
+  describe('Returns Invalid input if the value entered is not an array', function () {
+
+    it('should return Invalid input for a string', function () {
+      expect(myApp.findMinMax("[2,4,5,1]")).to.eql("Invalid input");
+    });
+
+    it('should return Invalid input for the boolean true', function () {
+      expect(myApp.findMinMax(true)).to.eql("Invalid input");
+    });
+
+  });
+
+  describe('Returns an empty array if the value entered is an empty array', function () {
+    it('should return [] for []', function () {
+      expect(myApp.findMinMax([])).to.eql([]);
+    });
+  });
+
+});
